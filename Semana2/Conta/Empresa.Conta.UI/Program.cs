764fd94c9e0eaf5c09b9dd4a9e5d98c20d72aaa8
keramgttmp <keramgttmp@gmail.com>
2020-09-30 21:04:52 -0600

    Semana 1 y 2
    Primeras dos semanas de curso. Semana 2 aplicación de consola y aplicación web tipo plantilla.

---
+++ Semana2/Conta/Empresa.Conta.UI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Empresa.Conta.Data;
+
+namespace Empresa.Conta.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductD pD = new ProductD();
+
+            var lista = pD.GetList();
+
+            Console.WriteLine("=================================================================================");
+            Console.WriteLine("******************** Prueba de Aplicación de Consola ****************************");
+            Console.WriteLine("=================================================================================");
+            Console.WriteLine("Listado de Productos");
+            foreach (var p in lista)
+            {
+                Console.WriteLine($"Id = {p.Id}, Nombre: {p.Nombre}. Precio {p.Precio} ");
+            }
+            Console.WriteLine("Fin de Listado");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ semana1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace semana1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
