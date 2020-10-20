9f1ef020385e367503eb377766ac6a4adcbd12fd
keramgttmp <keramgttmp@gmail.com>
2020-10-19 21:15:01 -0600

    Laboratorio 1 Acceso a datos
    Laboratorio 1 para práctica de acceso a datos. 3 proyectos: Data, Model , UI
    Realizado hasta el punto 9

---
+++ Semana5-1/NorthwindStore/NorthwindStore/Northwind.Store.UI.Shell/Program.cs
@@ -0,0 +1,27 @@
+﻿using Northwind.Store.Data;
+using Northwind.Store.Model;
+using System;
+using System.Linq;
+
+namespace Northwind.Store.UI.Shell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //vpy por el punto 9
+            using (var db = new NWContext())
+            {
+
+                var vQuery = db.Customers;
+
+                foreach (var p in vQuery)
+                {
+                    Console.WriteLine($"{p.CompanyName}");
+                }
+
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
