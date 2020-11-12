6a0f8ea3b793b26420cf1dbde28b5e3c6e89ecd8
keramgttmp <keramgttmp@gmail.com>
2020-11-11 22:21:33 -0600

    Semena 8-1 Primer QUIZ
    Operaciones CRUD con EF Net Core MVC

---
+++ Semana 8-1/Quiz1/S_Quiz/P_Quiz/Controllers/ToDoController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using P_Quiz;
+using P_Quiz.ViewModels;
+
+namespace P_Quiz.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly QuizContext _context;
+
+        public ToDoController(QuizContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ToDo
+        public IActionResult Index(ToDoViewModel tvm)
+        {
+
+            var filter = HttpContext.Session.GetString(nameof(tvm.Filter));
+
+            tvm.Filter = (tvm.Filter ?? filter) ?? "";
+            HttpContext.Session.SetString(nameof(tvm.Filter), tvm.Filter);
+
+            if (tvm.Filter != filter)
+            {
+                tvm.Page = 1;
+            }
+
+            var result = new List<ToDo>();
+
+            tvm.TotalCount = _context.ToDo.Where(t => t.Title.ToUpper().Trim().Contains(tvm.Filter)).Count();
+
+            result = _context.ToDo.Where(t => t.Title.ToUpper().Trim().Contains(tvm.Filter)).OrderBy(p => p.Id).Skip((tvm.Page - 1) * tvm.PageSize).
+                    Take(tvm.PageSize).ToList();
+
+
+            //var vm = new ProductIndexViewModel() { Products = result };
+            tvm.ToDo = result;
+
+            return View(tvm);
+        }
+
+        // GET: ToDo/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var toDo = await _context.ToDo
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (toDo == null)
+            {
+                return NotFound();
+            }
+
+            return View(toDo);
+        }
+
+        // GET: ToDo/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ToDo/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Due")] ToDo toDo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(toDo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(toDo);
+        }
+
+        // GET: ToDo/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var toDo = await _context.ToDo.FindAsync(id);
+            if (toDo == null)
+            {
+                return NotFound();
+            }
+            return View(toDo);
+        }
+
+        // POST: ToDo/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Due")] ToDo toDo)
+        {
+            if (id != toDo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(toDo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ToDoExists(toDo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(toDo);
+        }
+
+        // GET: ToDo/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var toDo = await _context.ToDo
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (toDo == null)
+            {
+                return NotFound();
+            }
+
+            return View(toDo);
+        }
+
+        // POST: ToDo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var toDo = await _context.ToDo.FindAsync(id);
+            _context.ToDo.Remove(toDo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ToDoExists(int id)
+        {
+            return _context.ToDo.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Semana 8-1/Quiz1/S_Quiz/P_Quiz/ViewModels/ToDoViewModel.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using P_Quiz.Models;
+
+namespace P_Quiz.ViewModels
+
+{
+    public class ToDoViewModel
+    {
+        public string Filter { get; set; }
+        public List<ToDo> ToDo{ get; set; }
+
+        public int TotalCount { get; set; }
+        public int PageSize => 4;
+        public int PageCount => TotalCount / PageSize;
+
+        /// <summary>
+        /// Página actual
+        /// </summary>
+        public int Page { get; set; } = 1;
+    }
+}
